2936d42190978cb0bf207fe1691cc68ac0b419e4
이지호 <cctv1998@naver.com>
2022-08-03 16:38:18 +0900

    feat: 구글 애널리틱스 연결

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -45,6 +45,24 @@ export default class MyDocument extends Document {
             type="text/css"
             href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
           />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          ></script>
+          <script
+            id="google-analytics"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
